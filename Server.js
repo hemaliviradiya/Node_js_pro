13e28ee4740049caadda68ed07f2a625393cbda3
Hemali_Viradiya14 <113543974+hemaliviradiya@users.noreply.github.com>
2023-07-27 11:14:41 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,8 +1,33 @@
 const express = require('express')
 const app = express()
 
+app.use("/page1", (req, res, next) => {
+    console.log(req.url + " requested")
+    next()
+})
+
 app.get("/", (req, res) => {
     res.send('Hello World')
+})
+
+app.get("/page1", (req, res, next) => {
+    //console.log("page1 called")
+    res.write("handler1 \n")
+    next()
+}, (req, res) => {
+    res.write("handler2")
+    res.end()
+})
+
+app.get("/page2", (req, res, next) => {
+    res.write("handler 3 \n")
+    next()
+}, (req, res, next) => {
+    res.write("handler 4 \n")
+    next()
+}, (req, res) => {
+    res.write("handler 5 ")
+    res.end()
 })
 
 app.listen(8000, () => {
